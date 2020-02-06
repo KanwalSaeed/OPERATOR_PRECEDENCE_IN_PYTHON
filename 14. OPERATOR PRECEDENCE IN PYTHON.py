9d95c26b71b934152586d74bdf3ceab5a42286b6
KanwalSaeed <kanwal_saeed122@hotmail.com>
2020-02-06 19:46:26 +0500

    first code

---
+++ 14. OPERATOR PRECEDENCE IN PYTHON.py
@@ -0,0 +1,19 @@
+
+
+print(5+4)
+print(3-2)
+print(25/5)
+print(4*2)
+print(3**2)
+
+profit = 10 + 20 * 2
+print(profit)
+
+profit = (10 + 20) * 2
+print(profit)
+
+profit = (10 + 20) * 2 - 30
+print(profit)
+
+profit = (10 + 20) * (2 - 30)
+print(profit)
